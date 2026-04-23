bf93c5a9b96f63ea6f0242f84ee208c5ef6168e3
Windlinxy <2248461514@qq.com>
2026-04-23 01:44:50 +0000

    Update MainApplication and MainActivity to use traditional React Native API

--- android/app/src/main/java/com/focusone/MainActivity.kt
+++ android/app/src/main/java/com/focusone/MainActivity.kt
@@ -2,15 +2,11 @@ package com.focusone
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
-import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
   override fun getMainComponentName(): String = "FocusOne"
 
-  override fun createReactActivityDelegate(): ReactActivityDelegate = DefaultReactActivityDelegate(
-    this,
-    mainComponentName,
-    fabricEnabled
-  )
+  override fun createReactActivityDelegate(): ReactActivityDelegate {
+    return ReactActivityDelegate(this, mainComponentName)
+  }
 }
